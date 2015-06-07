862c5e8870ae6753e90e5c4ad0c50d44fadd3c03
Neelabh Gupta <gupta.neelabh@gmail.com>
2015-06-07 01:40:20 -0500

    Update generated project files by comparing the latest changes in the Yeoman gulp-webapp generator (https://github.com/yeoman/generator-gulp-webapp/compare/v0.3.0...8702564).

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('styles', function () {
-  return gulp.src('app/styles/main.css')
+  return gulp.src('app/styles/*.css')
     .pipe($.sourcemaps.init())
     .pipe($.postcss([
       require('autoprefixer-core')({browsers: ['last 1 version']})
@@ -23,13 +23,18 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('.tmp/scripts'));
 });
 
-gulp.task('jshint', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
-});
+function jshint(files) {
+  return function () {
+    return gulp.src(files)
+      .pipe(reload({stream: true, once: true}))
+      .pipe($.jshint())
+      .pipe($.jshint.reporter('jshint-stylish'))
+      .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+  };
+}
+
+gulp.task('jshint', jshint('app/scripts/**/*.js'));
+gulp.task('jshint:test', jshint('test/spec/**/*.js'));
 
 gulp.task('html', ['styles', 'scripts'], function () {
   var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
@@ -46,13 +51,14 @@ gulp.task('html', ['styles', 'scripts'], function () {
 
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
-    })))
+    }))
+    .on('error', function(err){ console.log(err); this.end; })))
     .pipe(gulp.dest('dist/images'));
 });
 
@@ -100,6 +106,34 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], function () {
   gulp.watch('app/scripts/**/*.coffee', ['scripts']);
   gulp.watch('app/fonts/**/*', ['fonts']);
   gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+gulp.task('serve:test', function () {
+  browserSync({
+    notify: false,
+    open: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test'
+    }
+  });
+
+  gulp.watch([
+    'test/spec/**/*.js',
+  ]).on('change', reload);
+
+  gulp.watch('test/spec/**/*.js', ['jshint:test']);
 });
 
 // inject bower components
